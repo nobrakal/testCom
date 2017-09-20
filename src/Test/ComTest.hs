0459ce0bbe86264f25efa58263dbc9b722034cf9
Alexandre Moine <nobrakal@cthugha.org>
2017-09-20 12:47:50 +0200

    TestT

--- src/Test/ComTest.hs
+++ src/Test/ComTest.hs
@@ -4,14 +4,21 @@ module TestCom
     ( runTests
     ) where
 
+data TestT = TestT {
+  testsN :: [[String]] ,
+  testF :: String
+}
+
 runTests :: FilePath -> IO Bool
 runTests fp = do
   file <- readFile fp
+  let comm = keepCommentsAndFollowing (lines file) False [TestT [[]] ""]
   return True
 
-keepCommentsAndFollowing :: [String] -> Bool -> [[String]]
-keepCommentsAndFollowing [] _ = [[]]
-keepCommentsAndFollowing (x:xs) b
+keepCommentsAndFollowing :: [String] -> Bool -> [TestT] -> [TestT]
+keepCommentsAndFollowing [] _ tab = tab
+keepCommentsAndFollowing (x:xs) b tab
   | foldl (\x y -> x == " " && y) True x = keepCommentsAndFollowing xs b --Remove empty lines
-  | "--" `isPrefixOf` x = words (drop 2 x) : keepCommentsAndFollowing xs True
-  | b = words x : keepCommentsAndFollowing xs False
+  | "--" `isPrefixOf` x = keepCommentsAndFollowing xs True $ head tab {testsN = testsN tab ++ words x} : tab
+  | b = keepCommentsAndFollowing xs False $ head tab {testF = x} : tail tab
+  | otherwise  = keepCommentsAndFollowing xs b
