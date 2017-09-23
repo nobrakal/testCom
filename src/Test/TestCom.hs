35b998b96df7b9fd78b206fef2d091459e6b41a7
Alexandre Moine <nobrakal@cthugha.org>
2017-09-23 19:59:33 +0200

    IT WORKS

--- src/Test/TestCom.hs
+++ src/Test/TestCom.hs
@@ -2,9 +2,8 @@
 
 module Test.TestCom
     ( TestT (..),
-  --  runTests,
-  --  buildTests,
-    makeAllTests
+    makeAllTests,
+    getTestT
     ) where
 
 import Language.Haskell.TH
@@ -14,23 +13,18 @@ import Data.Either
 
 data TestT = TestT {
   valueTest :: [[String]], -- list of list of args
-  resTest :: [String],
+  resTest :: [String], -- list of results
   testF :: String,
   actualU :: Int
-}
+} deriving (Show)
 
-makeAllTests :: [TestT] -> [Q Dec]
-makeAllTests tab = nd : funcs
-  where
-    funcs = buildTests tab
-    fname = mkName "_TEST_runAllTests"
-    funcs_runned = appRecDec funcs
-    ex = appE (appE ([e|(++)|]) (appE [e|unlines|] (appE [e|builFinalString|] (listE funcs_runned)))) (([e|"TOTAL PASSED: " ++ show countRight' ++ "/"++ show length'|]))
-    cr = valD (varP (mkName "countRight'")) (normalB (appE [e|countRight|] (listE funcs_runned))) []
-    len = valD (varP (mkName "length'")) (normalB (appE [e|length|] (listE funcs_runned))) []
-    boo = [e|countRight' == length' |]
-    fClause = clause [] (normalB (tupE [ex,boo])) [cr,len]
-    nd = funD fname [fClause]
+makeAllTests :: Q [Dec]
+makeAllTests = do
+  loc <- location >>= (\(Loc y _ _ _ _) -> return y)
+  file <- runIO $ readFile loc
+  funcs <- sequenceQ (buildTests (getTestT file))
+  nd <- runTests loc $ appRecDec $ funcs
+  return (nd : funcs)
 
 buildTests' :: TestT -> [Q Dec]
 buildTests' (TestT [] _ _ _) = []
@@ -54,7 +48,17 @@ buildTests' x@(TestT valueTest' resTest' testF' actualU') = nd : buildTests' nxs
 
 buildTests :: [TestT] -> [Q Dec]
 buildTests [] = []
-buildTests (x:xs) = buildTests' x ++ buildTests xs
+buildTests (x:xs) = (buildTests' x) ++ (buildTests xs)
+
+runTests :: String -> [Q Exp] -> Q Dec
+runTests str funcs_runned = funD fname [fClause]
+  where
+    fname = mkName $ "_TEST_"++ take ((length str)-3) (replaceSlashByUnderscore str)
+    ex = appE (appE ([e|(++)|]) (appE [e|unlines|] (appE [e|builFinalString|] (listE funcs_runned)))) (([e|"TOTAL PASSED: " ++ show countRight' ++ "/"++ show length'|]))
+    cr = valD (varP (mkName "countRight'")) (normalB (appE [e|countRight|] (listE funcs_runned))) []
+    len = valD (varP (mkName "length'")) (normalB (appE [e|length|] (listE funcs_runned))) []
+    boo = [e|countRight' == length' |]
+    fClause = clause [] (normalB (tupE [ex,boo])) [cr,len]
 
 appRec :: ([String],Q Exp) -> Q Exp
 -- appRec [] = return
@@ -62,9 +66,9 @@ appRec ([],a) = a
 appRec ((x:xs),b) = appE (appRec (xs,b)) (litE $ integerL (read x))
 
 -- Run all declarations and store them into a tab
-appRecDec :: [Q Dec] -> [Q Exp]
+appRecDec :: [Dec] -> [Q Exp]
 appRecDec [] = []
-appRecDec (x:xs) = (x >>= \y -> (varE (getName y))) : appRecDec xs
+appRecDec (x:xs) = (varE (getName x)) : appRecDec xs
 
 getName :: Dec -> Name
 getName (FunD name _ ) = name
@@ -75,3 +79,25 @@ builFinalString (x:xs) = (either ("Error: " ++ ) ("Test passed: " ++) x ): builF
 
 countRight :: [Either a b] -> Int
 countRight z = foldl (\x y -> if isLeft y then x else x+1) (0 :: Int) z
+
+getTestT :: String -> [TestT]
+getTestT str = getTestT' (lines str) False (TestT [] [] [] 0)
+
+-- t is supposed non empty
+getTestT' :: [String] -> Bool -> TestT -> [TestT]
+getTestT' [] _ _ = []
+getTestT' (x:xs) b t
+  | "--[" `isPrefixOf` x && "]" `isSuffixOf` x = getTestT' xs True $ t {valueTest = args : (valueTest t), resTest = res : (resTest t)}
+  | not (null $ words x) && not ("--" `isPrefixOf` hw) && b = t {testF = hw} : getTestT' xs False (TestT [[]] [] [] 0)
+  | otherwise = getTestT' xs b t
+  where
+    list = words $ drop 3 (init x)
+    args = init list
+    res = last list
+    hw = head (words x)
+
+replaceSlashByUnderscore :: String -> String
+replaceSlashByUnderscore [] = []
+replaceSlashByUnderscore (x:xs)
+  | x == '/' = '_':replaceSlashByUnderscore xs
+  | otherwise = x : replaceSlashByUnderscore xs
--- tests/Main.hs
+++ tests/Main.hs
@@ -3,19 +3,19 @@ module Main where
 
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
-import PseudoMacros
 import System.Exit
 
 import Test.TestCom
 
-$(sequenceQ $ makeAllTests [TestT [["1","2"]] ["4"] "add" 0])
+$(makeAllTests)
 
--- Tests
 main :: IO ()
 main = do
-  let (str,res) = _TEST_runAllTests
+  let (str,res) = _TEST_tests_Main
   putStrLn str
   if res then exitSuccess else exitFailure
+  -- print $ getTestT file
 
+--[1 2 3]
 add :: Int -> Int -> Int
 add x y = x+y
