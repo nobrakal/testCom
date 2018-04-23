b85d1ed735d34baf9f20a83729482613e3aa9687
Alexandre Moine <alexandre@moine.me>
2018-04-23 12:54:35 +0200

    Cleanup

--- src/Test/TestCom/Parser/Parser.hs
+++ src/Test/TestCom/Parser/Parser.hs
@@ -7,7 +7,7 @@ import qualified Text.Parsec.Combinator
 import Text.Parsec.Char (endOfLine)
 import Control.Applicative
 import Control.Monad
-import Data.Semigroup
+import Data.Semigroup hiding (option)
 
 import Test.TestCom.Type
 
@@ -17,38 +17,36 @@ a <||> b = (try a) <|> b
 inner :: Parser String
 inner = string "[" <> many (noneOf "[]") <> (lookAhead (string "]") <|> inner ) <> string "]"
 
-testUnit :: Parser TestUnit
-testUnit =      (lookAhead (char '[') *> normal)
-           <||> (char 'O' *> override)
-           <||> (char 'S' *> spec)
+starter :: Parser TestType
+starter = (lab >> return Normal)
+           <||> (char 'O' *>lab >> return Override)
+           <||> (char 'S' *> lab >> return Spec)
   where
-    normal = do
-      args <- inner
-      skipMany space
-      result <- inner
-      return $ TestUnit Normal args result 1
-    override = do
-      args <- inner
-      skipMany space
-      result <- inner
-      return $ TestUnit Override args result 1
-    spec = do
-      args <- inner
-      skipMany space
-      result <- inner
-      skipMany space
-      nb <- ((\x -> read x :: Int) <$> inner) <||> (return 1)
-      return $ TestUnit Spec args result nb
+    lab = lookAhead (char '[') 
+
+
+testUnit :: TestType -> Parser TestUnit
+testUnit tt = do
+    args <- inner
+    skipMany space
+    result <- inner
+    case tt of
+      Normal -> return $ TestUnit Normal args result 1
+      Override -> return $ TestUnit Override args result 1
+      Spec -> do
+        skipMany space
+        nb <- option 1 $ (\x -> read x :: Int) <$> inner
+        return $ TestUnit Spec args result nb
 
 tests :: Parser [Test]
 tests = notTest *> sepEndBy test notTest
   where
-    notTest = manyTill (anyChar) (lookAhead test) 
+    notTest = manyTill anyChar (lookAhead test) 
 
 test :: Parser Test
 test = do
-  tests <- string "--" *> testUnit `endBy` endOfLine 
-  func <- manyTill (anyChar) (try (string " ")) <* (many (noneOf "\n") >> endOfLine)
+  tests <- (string "--" *> starter >>= testUnit) `endBy` endOfLine 
+  func <- manyTill anyChar (try (string " ")) <* (many (noneOf "\n") >> endOfLine)
   return $ Test tests func 0
 
 getTestT :: String -> IO (Either ParseError [Test])
